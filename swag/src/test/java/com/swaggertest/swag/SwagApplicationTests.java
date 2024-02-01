fb9bd4e7246829d29761f264bf0a8cc8c2f8e44f
임동훈 <snowcrab382@naver.com>
2024-02-01 10:40:46 +0900

    add demo spring project

---
+++ swag/src/test/java/com/swaggertest/swag/SwagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swaggertest.swag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
